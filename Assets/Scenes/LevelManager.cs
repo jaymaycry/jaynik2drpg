8094bc47cc8067fb53c86d604c687e9ba848fb64
Dominik Werner <dowerner@student.ethz.ch>
2016-02-11 01:07:36 +0100

    Added test UI.

---
+++ Assets/Scenes/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+[InitializeOnLoad]
+public class LevelManager : MonoBehaviour {
+
+    public Button LoadButton;
+
+    static LevelManager()
+    {
+        Debug.Log("Game started...");
+    }
+
+	// Use this for initialization
+	void Start () {
+        LoadButton.onClick.AddListener(() => { test(); });
+    }
+
+    void test()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
